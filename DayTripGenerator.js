3cdb6dd8d24d5bed44549bacbdb1c3ca2c03ccea
dylanbram354 <dylanbram354@gmail.com>
2021-04-30 16:08:04 -0500

    Done for the day. Cleaned up some things; removed old notes.

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -4,21 +4,6 @@ let rests = ["Bob's Burgers", "Los Pollos Hermanos", "Jack Rabbit Slim's", "Pizz
 let transps = ["Tesla","space shuttle","unicycle","magic carpet","camel"];
 let ents = ["juggling show", "Renaissance fair", "sword-swallowing convention", "underwater basket weaving world championships"];
 
-/*
-1) Create a function to randomly select a destination. It will not take in any parameters.
-2) Create an array containing all possible destinations.
-3) Generate a random number from 1 - array.length using Math.random() and Math.floor
-4) return array[randomNumber]
-5) repeat for each other criteria
-**Used one function to select random element from given array**
-
-6) Create a function (no parameters) that calls each of the other functions and returns a string with the day trip info
-7) Call the above function and save to variable 
-8) Create a variable for userAnswer. Set equal to prompt with day trip info and ask if they're happy (YES or NO)
-9) While userAnswer = no, call the function again, and run prompt again
-**Needed to refactor to allow user to select which element(s) to change!!**
-10) Print final result to console.
-*/
 
 function randomChoice(optionsArray){
     let amount = optionsArray.length;
@@ -42,7 +27,7 @@ let userChoice = prompt(firstPrompt);
 
 while (userChoice !== "YES"){
     if (userChoice.includes("DESTINATION")){
-        newDest = randomChoice(dests);
+        randomDest = randomChoice(dests);
     }
     if (userChoice.includes("RESTAURANT")){
          randomRest = randomChoice(rests);
