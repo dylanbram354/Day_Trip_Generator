d999f919d44983275f7b383e689b0e9505bcfd58
dylanbram354 <dylanbram354@gmail.com>
2021-04-30 17:10:01 -0500

    Created eliminateChoice function to avoid elements being suggested more than once. Also prompt users to enter own choice if they run out of options.

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -23,27 +23,48 @@ let dayTripStatement = "We will be travelling to "+randomDest+", where will dine
 let firstPrompt = "Hello dear user! Here is your day trip: \n\n"+dayTripStatement+"\n\nDoes that sound good? "+
 "If so, enter YES. If not, enter the elements you would like to change in ALL CAPS, separated by one space. You may choose to change DESTINATION, RESTAURANT, TRANSPORT, and/or ENTERTAINMENT.";
 
-let userChoice = prompt(firstPrompt);
+// function doTheThing(){ //
+    let userChoice = prompt(firstPrompt);
 
-while (userChoice !== "YES"){
-    if (userChoice.includes("DESTINATION")){
-        randomDest = randomChoice(dests);
+    while (userChoice !== "YES"){
+        if (userChoice.includes("DESTINATION")){//
+            dests = eliminateChoice(dests,randomDest);
+            randomDest = randomChoice(dests);
+            if (dests.length === 0){
+                randomDest = prompt("Well fine then, if you're so picky, enter your own destination!");
+            }
+        }
+        if (userChoice.includes("RESTAURANT")){
+            rests = eliminateChoice(rests,randomRest);
+            randomRest = randomChoice(rests);
+            if (rests.length === 0){
+                randomRest = prompt("Well fine then, if you're so picky, enter your own restaurant!");
+            }
+        }
+        if (userChoice.includes("TRANSPORT")){
+            transps = 
+            randomTransp = randomChoice(transps);
+        }
+        if (userChoice.includes("ENTERTAINMENT")){
+            randomEnt = randomChoice(ents);
+        }
+        dayTripStatement = "We will be travelling to "+randomDest+", where will dine at "+randomRest+
+        ". Afterwards, we will travel by "+randomTransp+" to enjoy the "+randomEnt+".";
+        userChoice = prompt("Very well.. what about this? \n\n"+dayTripStatement+
+        "\n\nIf you approve, enter YES. Otherwise, enter the elements you'd like to change (DESTINATION, RESTAURANT, TRANSPORT, and/or ENTERTAINMENT).")
     }
-    if (userChoice.includes("RESTAURANT")){
-         randomRest = randomChoice(rests);
-    }
-    if (userChoice.includes("TRANSPORT")){
-        randomTransp = randomChoice(transps);
-    }
-    if (userChoice.includes("ENTERTAINMENT")){
-        randomEnt = randomChoice(ents);
-    }
-    dayTripStatement = "We will be travelling to "+randomDest+", where will dine at "+randomRest+
-    ". Afterwards, we will travel by "+randomTransp+" to enjoy the "+randomEnt+".";
-    userChoice = prompt("Very well.. what about this? \n\n"+dayTripStatement+
-    "\n\nIf you approve, enter YES. Otherwise, enter the elements you'd like to change (DESTINATION, RESTAURANT, TRANSPORT, and/or ENTERTAINMENT).")
+
+    console.log("Excellent choice! " + dayTripStatement +" Enjoy your trip!");
+
+//
+
+function eliminateChoice(optionsArray, currentPick){
+    let newOptions = [];
+        for(let i=0; i<optionsArray.length; i++){
+            if (optionsArray[i] !== currentPick){
+                newOptions.push(optionsArray[i]);
+            }
+        }
+        optionsArray = newOptions;
+        return newOptions;
 }
-
-console.log("Excellent choice! " + dayTripStatement +" Enjoy your trip!");
-
-
